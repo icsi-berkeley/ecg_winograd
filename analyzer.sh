82a51cb47d08b2cb91d10b83d583b6f8f5fa25b2
vivekraghuram <VV9295@YAHOO.COM>
2016-09-27 11:42:42 -0700

    Remove submodules

--- analyzer.sh
+++ analyzer.sh
@@ -1,3 +1,3 @@
 export ECG_FED=FED1
-export JYTHONPATH=framework_code/build/compling.core.jar:framework_code/src/main/nluas/language
-jython -m analyzer ecg_grammars/winograd.prefs
+export JYTHONPATH=../ecg_framework_code/build/compling.core.jar:../ecg_framework_code/src/main/nluas/language
+jython -m analyzer ../ecg_grammars/winograd.prefs
